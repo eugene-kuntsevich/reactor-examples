659caff997ab1c2235dd9ba20e6b60379349a65d
Evgeny Kuntsevich <evgeny.kuntsevich-ext@enfuce.com>
2023-12-01 08:41:46 +0300

    initial commit

---
+++ src/main/java/com/example/reactorexamples/ReactorExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactorexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactorExamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactorExamplesApplication.class, args);
+    }
+
+}
